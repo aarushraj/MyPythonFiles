38c808bb8a229a8aabbb4c5873344bdca0da3c3b
Aarush Raj Agarwal <aarushagarwal1011@gmail.com>
2024-10-17 13:51:00 +0530

    Update CreatingTriangle.py

--- CreatingTriangle.py
+++ CreatingTriangle.py
@@ -2,14 +2,14 @@ import turtle
 
 canvas = turtle.Turtle()
 
-canvas.forward(150) # draw base
+canvas.forward(300) # draw base
 
 canvas.left(120)
 
-canvas.forward(150)
+canvas.forward(300)
 
 canvas.left(120)
 
-canvas.forward(150)
+canvas.forward(300)
 
 turtle.done()
